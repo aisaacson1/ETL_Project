049ed191f9e74c31dfcbb848690023f22cf6265f
aisaacson1 <50605527+aisaacson1@users.noreply.github.com>
2019-12-14 16:17:41 -0500

    Adding scripts for loading data into MySQL on AWS

---
+++ config.py
@@ -0,0 +1,9 @@
+#****AWS*****
+local_gwsis_dbname = 'gwsis'
+local_gwsis_dbuser = 'admin'
+local_gwsis_dbpwd = 'JBob2005'
+remote_db_endpoint = 'databootcamp.cv4kwcthnpfl.us-east-2.rds.amazonaws.com'
+remote_db_port = 3306
+remote_gwsis_dbuser = 'admin'
+remote_gwsis_dbpwd = 'JBob2005'
+remote_gwsis_dbname = 'gwsis'
